320eda31dcf4495ca3bf232c2a2c4bb19139a749
Pavel <pavelfilchi@gmail.com>
2024-05-18 12:16:36 +0200

    Adding GUI

--- tiktok-auto-downloader.py
+++ tiktok-auto-downloader.py
@@ -1,3 +1,4 @@
+import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
@@ -5,8 +6,23 @@ import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import os
+import tkinter as tk
+from tkinter import messagebox
+import configparser
+
+stop_download = False  # Global variable to control the stopping of the download process
+
+def exitProgram():
+    if messagebox.askokcancel("Exit", "Are you sure you want to exit?"):
+        root.destroy()
+def log_message(message):
+    log_text.insert(tk.END, message + "\n")
+    log_text.see(tk.END)
 
 def downloadVideo(link, id):
+    global stop_download
+    if stop_download:
+        return
     print(f"Downloading video {id} from: {link}")
     cookies = {
       # read the README file to understand how to get the cookies
@@ -25,14 +41,14 @@ def downloadVideo(link, id):
         #read the README file to understand how to get the data
     }
 
-    print("Getting the download link")
+    log_message("Getting the download link")
     response = requests.post('https://ssstik.io/abc', params=params, cookies=cookies, headers=headers, data=data)
     downloadSoup = BeautifulSoup(response.text, "html.parser")
 
     downloadLink = downloadSoup.a["href"]
     videoTitle = downloadSoup.p.getText().strip()
 
-    print("Saving the video in the folder")
+    log_message("Saving the video in the folder")
     mp4File = urlopen(downloadLink)
     # Check if the directory exists, if not, create it
     video_directory = "video"
@@ -48,44 +64,120 @@ def downloadVideo(link, id):
             else:
                 break
 
-print("Opening the Chrome browser")
-options = Options()
-options.add_argument("start-maximized")
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-driver = webdriver.Chrome(options=options)
-# Change the tiktok link to the channel you need
-driver.get("https://www.tiktok.com/@example")
 
-# Change this value if you have a Captcha (put just enough time to solve it)
-time.sleep(60)
+# Function to start the download process
+def startDownload():
+    global stop_download
+    stop_download = False  # Reset stop_download flag at the beginning of the download
+    channel_link = entry_channel_link.get()
+    class_name = entry_class_name.get()
 
-scroll_pause_time = 1
-screen_height = driver.execute_script("return window.screen.height;")
-i = 1
+    def download_thread():
+        log_message("Opening the Chrome browser")
+        options = Options()
+        options.add_argument("start-maximized")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        driver = webdriver.Chrome(options=options)
+        driver.get(channel_link)
 
-print("Scrolling page to get all the videos...")
-while True:
-    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
-    i += 1
-    time.sleep(scroll_pause_time)
-    scroll_height = driver.execute_script("return document.body.scrollHeight;")  
-    if (screen_height) * i > scroll_height:
-        break 
+        remaining_time = 30
 
-#read the README file to understand how to get the className
-className = "css-example-example example"
+        while remaining_time > 0:
+            log_message(f"Waiting for {remaining_time} seconds...")
+            time.sleep(1)
+            remaining_time -= 1
 
-script  = "let l = [];"
-script += "document.getElementsByClassName(\""
-script += className
-script += "\").forEach(item => { l.push(item.querySelector('a').href)});"
-script += "return l;"
+        scroll_pause_time = 1
+        screen_height = driver.execute_script("return window.screen.height;")
+        i = 1
 
-urlsToDownload = driver.execute_script(script)
+        log_message("Scrolling page to get all the videos...")
 
-print(f" {len(urlsToDownload)} Videos found")
-for index, url in enumerate(urlsToDownload):
-    print(f"Downloading video: {index}")
-    downloadVideo(url, index)
-    time.sleep(10)
+        while True:
+            if stop_download:
+                driver.quit()
+                return
+            driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
+            i += 1
+            time.sleep(scroll_pause_time)
+            scroll_height = driver.execute_script("return document.body.scrollHeight;")
+            if (screen_height) * i > scroll_height:
+                break
+
+        script = "let l = [];"
+        script += "Array.from(document.getElementsByClassName(\""
+        script += class_name
+        script += "\")).forEach(item => { l.push(item.querySelector('a').href)});"
+        script += "return l;"
+
+        urlsToDownload = driver.execute_script(script)
+
+        log_message(f" {len(urlsToDownload)} Videos found")
+        for index, url in enumerate(urlsToDownload):
+            if stop_download:
+                break
+            log_message(f"Downloading video: {index}")
+            downloadVideo(url, index)
+            time.sleep(10)
+
+        driver.quit()
+
+    thread = threading.Thread(target=download_thread)
+    thread.start()
+
+
+def stopDownload():
+    global stop_download
+    stop_download = True
+    log_message("Download stopped")
+
+
+def saveConfig():
+    config = configparser.ConfigParser()
+    config['DEFAULT'] = {
+        'channel_link': entry_channel_link.get(),
+        'class_name': entry_class_name.get()
+    }
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+    messagebox.showinfo("Info", "Configuration saved successfully!")
+
+
+def loadConfig():
+    config = configparser.ConfigParser()
+    if os.path.exists('config.ini'):
+        config.read('config.ini')
+        entry_channel_link.insert(0, config['DEFAULT'].get('channel_link', ''))
+        entry_class_name.insert(0, config['DEFAULT'].get('class_name', ''))
+
+
+root = tk.Tk()
+root.title("TikTok Video Downloader")
+
+tk.Label(root, text="Channel Link:").grid(row=0, column=0, sticky=tk.W)
+entry_channel_link = tk.Entry(root, width=50)
+entry_channel_link.grid(row=0, column=1, padx=10, pady=5)
+
+tk.Label(root, text="Class Name:").grid(row=1, column=0, sticky=tk.W)
+entry_class_name = tk.Entry(root, width=50)
+entry_class_name.grid(row=1, column=1, padx=10, pady=5)
+
+log_text = tk.Text(root, height=20, width=80)
+log_text.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
+log_text.tag_configure("red", foreground="red")
+
+button_exit = tk.Button(root, text="Exit", command=exitProgram)
+button_exit.grid(row=6, column=3, padx=10, pady=10, sticky=tk.W)
+
+button_download = tk.Button(root, text="Download", command=startDownload)
+button_download.grid(row=6, column=1, padx=10, pady=10, sticky=tk.E)
+
+button_stop = tk.Button(root, text="Stop", command=stopDownload)
+button_stop.grid(row=6, column=2, padx=10, pady=10, sticky=tk.E)
+
+button_save = tk.Button(root, text="Save Config", command=saveConfig)
+button_save.grid(row=6, column=0, padx=10, pady=10, sticky=tk.W)
+
+loadConfig()
+root.mainloop()
